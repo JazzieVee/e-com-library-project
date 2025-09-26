9520aaa930e1efd5438d668efdce2fd55512fe9d
Jasmine <oatman.estates.holdings@gmail.com>
2025-09-26 15:06:13 -0400

    ecom project with react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+
+function App() {
+  return (
+    <Router> 
+    <div className="App">
+    <Nav />
+    <Route path="/" exact component={Home} />
+    <Route path="/books" render={() => <Books books={books} />} />
+    <Route path="/books/1" render={() => <BookInfo books={books} /> } />
+    <Footer />
+    </div>
+     </Router>
+  );
+}
+
+export default App;
